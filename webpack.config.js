5e690876768e032fbfcf17a991018d28799cd84b
technix <sergei.mozhaisky@gmail.com>
2016-12-21 13:52:28 +0200

    config: fix path

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'build'),
-        publicPath: '/',
+        publicPath: './',
         filename: 'app.js'
     },
     module: {
