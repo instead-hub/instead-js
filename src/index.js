ad0e0d3d7fa1b10d2669c939e08001d37fb2f1ae
Serhii Mozhaiskyi <serhii.mozhaiskyi@globallogic.com>
2016-12-21 10:47:42 +0200

    refactoring: component initialization

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-var INSTEAD = require('./app/instead');
+// load styles
+require('perfect-scrollbar/dist/css/perfect-scrollbar.css');
+require('./style.css');
+
+var Instead = require('./app/instead');
+var UI = require('./app/ui');
+var Menu = require('./app/menu');
 
 document.addEventListener(
     'DOMContentLoaded',
     function onLoad() {
-        INSTEAD.init();
-        INSTEAD.startGame();
+        // initialization of INSTEAD components
+        Instead.init();
+        Menu.init(UI.element, Instead.handlers);
+        UI.init(Instead.handlers);
+        // load and start game
+        Instead.startGame();
     }
 );
