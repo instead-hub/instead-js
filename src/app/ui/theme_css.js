8a427f527692dd6defb7d963d242a41efc683531
technix <sergei.mozhaisky@gmail.com>
2016-12-28 15:20:04 +0200

    theme: fix line-height for inventory

--- src/app/ui/theme_css.js
+++ src/app/ui/theme_css.js
@@ -159,7 +159,8 @@ var applyStyle = {
         e.$inventory.css('font-size', v + 'px');
     },
     'inv.fnt.height': function s(e, v) {
-        e.$inventory.css('line-height', v);
+        // default line height for browsers: 1.2
+        e.$inventory.css('line-height', +v * 1.2);
     },
 
     'menu.col.bg': function s(e, v) { e.$menu.css('background-color', v); },
