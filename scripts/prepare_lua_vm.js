637dfa75b8c950872a97bcbf344e1bea2db2de5d
techniX <sergei.mozhaisky@gmail.com>
2017-01-23 21:04:31 +0200

    Update weblua

--- scripts/prepare_lua_vm.js
+++ scripts/prepare_lua_vm.js
@@ -1,7 +1,5 @@
 var fs = require('fs');
 
 var lua = fs.readFileSync('./instead/git/weblua/src/lua.vm.js', 'utf-8');
-var outputFileContent = '(function(w){var Lua={cache:{}};var z={i:function(){' + lua + '}};z.i();' +
-    'Lua=z.Lua;Lua.cache.items={};Lua.cache.clear=function(a){delete Lua.cache.items[a]};' +
-    'w.Lua=Lua;})(window);';
+var outputFileContent = '(function(){' + lua + '})();';
 fs.writeFile('./instead/lua.vm.js', outputFileContent, {flag: 'w'});
