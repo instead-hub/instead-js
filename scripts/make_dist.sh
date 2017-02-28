99c7e83a0cf74eda0f7e7e3a9ed6f08e88191cbd
techniX <sergei.mozhaisky@gmail.com>
2017-02-28 10:16:27 +0200

    Update make_dist script

--- scripts/make_dist.sh
+++ scripts/make_dist.sh
@@ -7,15 +7,15 @@ DESTDIR="$DIST/$TMPDIR"
 rm -rf $DIST
 mkdir $DIST
 mkdir $DESTDIR
-cp "$SRCDIR/index.html" $DESTDIR
-cp "$SRCDIR/instead.js" $DESTDIR
-cp "$SRCDIR/instead.png" $DESTDIR
-cp "$SRCDIR/list_games.js" $DESTDIR
-cp "$SRCDIR/styles.css" $DESTDIR
-cp -r "$SRCDIR/themes" $DESTDIR
+cp $SRCDIR/*.html $DESTDIR
+cp $SRCDIR/*.js $DESTDIR
+cp $SRCDIR/*.json $DESTDIR
+cp $SRCDIR/*.css $DESTDIR
+cp $SRCDIR/*.png $DESTDIR
+cp -r $SRCDIR/themes $DESTDIR
 
-mkdir "$DESTDIR/games"
-cp -r "instead/git/instead/games/tutorial3" "$DESTDIR/games"
+mkdir $DESTDIR/games
+cp -r instead/git/instead/games/tutorial3 $DESTDIR/games
 (cd $DESTDIR && node list_games.js)
 (cd $DIST && zip -qr instead-js.zip instead-js)
 rm -rf $DESTDIR
