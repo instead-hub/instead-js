3c71b69ea38897f4098d04dd3914ee43cf9539a2
Sergei Mozhaisky <sergei.mozhaisky@gmail.com>
2018-09-07 16:54:29 +0300

    Fix 'require' and filesystem behavior

--- src/lua/instead_init.lua
+++ src/lua/instead_init.lua
@@ -1,3 +1,12 @@
+local requires = {}
+
+function require(fn)
+	if requires[fn] then return requires[fn] end
+	local code = js.run_string('Lua.requireContent("' .. tostring(fn) .. '")')
+	requires[fn] = loadstring(code)() or true
+	return requires[fn]
+end
+
 require 'instead_js'
 require 'stead'
 require 'ext/gui'
